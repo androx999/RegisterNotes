7076ef5f243f69366758000b1b851247e69971d4
Andrés Briseño <Andrs Briseo@DESKTOP-RQJD2V1>
2022-10-30 18:30:38 -0600

    Notes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Register',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
